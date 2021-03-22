51cda6c4d06b27d6cdbed03a27ad1916b55b3ac3
abu0sman <mag@spdag.ru>
2021-03-22 15:36:44 +0300

    Сруктура html файла для example файла

---
+++ example/index.php
@@ -0,0 +1,14 @@
+    <!DOCTYPE HTML>
+    <html lang="ru">
+      <head>
+        <meta charset="UTF-8">
+        <title>z_uploader example page</title>
+        <meta name="description" content="z_uploader example page" />
+        
+        <link rel="icon" href="/img/favicon.png">
+        <script type="text/javascript" src="https://code.jquery.com/jquery-latest.min.js"></script>
+      </head>
+      <body>
+        
+      </body>
+    </html>
